26ae4f967909674cd09c373c33fb7a069e5d1058
miljansavic13 <miljan.savic13@gmail.com>
2026-07-20 21:26:44 +0200

    chore(repo): add shared package manifests and configs

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{ts,tsx}'],
+  prefix: 'cf-',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
